eb35ec39058d92ba733ff8ef32403883dadfd13a
Your Name <you@example.com>
2024-03-20 15:29:05 +0300

    3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 
 import './styles/App.css';
-import Post from './Components/Post';
+
 import PostItem from './Components/PostItem';
 import { useState } from 'react';
+import PostList from './Components/PostList';
 
 function App() {
 const[posts,setPosts]=useState(
@@ -16,12 +17,22 @@ const[posts,setPosts]=useState(
   ]
 )
 
+const[posts2,setPosts2]=useState(
+  [
+    {id:0,title:"c++ 0",body:"c++ body"},
+    {id:1,title:"c++ 1",body:"c++ body"},
+    {id:2,title:"c++ 2",body:"c++ body"},
+    {id:3,title:"c++ 3",body:"c++ body"},
+    {id:4,title:"c++ 4",body:"c++ body"},
+    {id:5,title:"c++ 5",body:"c++ body"},
+  ]
+)
+
 
   return (
     <div className="App">
-      {posts.map(post =>
-     <PostItem post={post} key={post.id}/>
-      )}
+     <PostList posts={posts} title="Список постов 1"/>
+     <PostList posts={posts2} title="Список постов 2"/>
     </div>
   );
 }
